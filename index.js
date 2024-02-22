f898fd4b13857e3b7e2aefc8569deed9644c3d6b
Oscar Castro <ocastrobeltran@gmail.com>
2024-02-22 18:02:44 -0500

    Se añade envio de registros a base de datos phpmyadmin  y validacion de cedula repetida en base de datos

--- index.js
+++ index.js
@@ -22,33 +22,46 @@ app.get("/", function(req, res){
 app.post("/validate", function(req, res){ //Validate es la accion del form en el html
     const datos = req.body;
 
-        let nombre = datos.name; //aca definimos con el ID del input del form
-        let apellido = datos.lastname;
-        let cedula = datos.identification;
-        let telefono = datos.phone;
-        let correo = datos.email;
-        let deporte = datos.sport;
-        let fecha = datos.date;
-        let escenario = datos.scenary;
-        // let hora = datos.time;       //comentado mientras soluciono lo del js y css
-        let terminos = datos.terms;
+    let nombre = datos.name; //aca definimos con el ID del input del form
+    let apellido = datos.lastname;
+    let cedula = datos.identification;
+    let telefono = datos.phone;
+    let correo = datos.email;
+    let deporte = datos.sport;
+    let fecha = datos.date;
+    let escenario = datos.scenary;
+    // let hora = datos.time;       //comentado mientras soluciono lo del js y css
+    let terminos = datos.terms;
 
-        //FALTA INTEGRAR LA HORA hasta solucionar lo del js y css
-        //ACA INSERTAMOS EN LA TABLA LO VALRES MEDIANTE ESTA CONSULTA EN LA BD
-        let registrar = "INSERT INTO reservas (name, lastname, identification, email, phone, sport, scenary, date, termns) VALUE ('"+nombre+"', '"+apellido+"', '"+cedula+"', '"+correo+"', '"+telefono+"', '"+deporte+"', '"+escenario+"', '"+fecha+"' , '"+terminos+"')";
-        
-        //Aca se realiza la conexion dando la consulta en la BD
-        conexion.query(registrar, function(error){
-            if(error){
-                throw error;   //si hay error que me lo muestre
-            } else {
-                console.log("Datos almacenados correctamente");
+
+    // Con esta consulta validamos si la cedula ya fue registrada, si no fue registrada permite hacer el envio del form
+    let buscar = "SELECT * FROM reservas WHERE identification = "+cedula+" ";
+    conexion.query(buscar, function(error, row){
+        if(error){
+            throw error;   //si hay error que me lo muestre
+        } else {
+            if(row.length >0){ // Aca valida si hay mas 1 registro con la cedula ya registrado arrojará error
+                console.log("Usuario con numero de cedula ya existente, no se envia registro");
+            } else { 
+                //FALTA INTEGRAR LA HORA hasta solucionar lo del js y css
+                //ACA INSERTAMOS EN LA TABLA LO VALRES MEDIANTE ESTA CONSULTA EN LA BD
+                let registrar = "INSERT INTO reservas (name, lastname, identification, email, phone, sport, scenary, date, terms) VALUE ('"+nombre+"', '"+apellido+"', '"+cedula+"', '"+correo+"', '"+telefono+"', '"+deporte+"', '"+escenario+"', '"+fecha+"' , '"+terminos+"')";
+                
+                //Aca se realiza la conexion dando la consulta en la BD
+                conexion.query(registrar, function(error){
+                    if(error){
+                        throw error;   //si hay error que me lo muestre
+                    } else {
+                        console.log("Datos almacenados correctamente");
+                    }
+                });
             }
-        });
+        }
+    });   
 
     console.log(datos);
 });
 
 app.listen(3000, function() {
-    console.log("Servidor en el puerto 3000");
+    console.log("Servidor en el puerto 3000 http://localhost:3000/");
 });
